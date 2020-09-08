8ccf2d86dd99d8b10677f4364b05f35d725c0019
MaxHayday <filipenko.91@mail.ru>
2020-09-08 20:57:50 +0300

    Second example

--- src/main/java/com/max_hayday/algorithms/Task2.java
+++ src/main/java/com/max_hayday/algorithms/Task2.java
@@ -17,15 +17,16 @@ public class Task2 {
     public static void main(String[] args) {
         int[] array1 = new int[]{4, 5, 6, 7, 8};
         int[] array2 = new int[]{4, 5, 6, 6, 8};
-        System.out.println(findDuplicatesInArrayUsingTreeSet(array2));
+        System.out.println(findDuplicatesInArrayUsingTreeSet(array1));
     }
 
     public static boolean findDuplicatesInArrayUsingTreeSet(int[] array) {
         TreeSet treeSet = new TreeSet();
-        for (int i :
-                array) {
-            treeSet.add(i);
+        for (int i = 1; i <= array.length - 1; i++) {
+            treeSet.add(array[i]);
+            if (i > treeSet.size())
+                return true;
         }
-        return treeSet.size() == array.length ? false : true;
+        return false;
     }
 }
